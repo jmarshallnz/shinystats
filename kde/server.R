2a3d31c88c6dbce4ee5386f0153a562823d1bfae
Jonathan Marshall <jmarshall@never.you.mind>
2020-07-17 15:00:25 +1200

    density app to ggplot2

--- kde/server.R
+++ kde/server.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(ggplot2)
 
 # generate our data
 get_data <- function(n) {
@@ -6,7 +7,7 @@ get_data <- function(n) {
   nn <- as.numeric(rmultinom(1, n, prob=rep(1/n_m, n_m)))
   mu <- rep(rnorm(n_m, sd=3), times=nn)
   r <- rnorm(n) + mu
-  r
+  data.frame(r=r)
 }
 
 shinyServer(function(input, output, session) {
@@ -18,19 +19,15 @@ shinyServer(function(input, output, session) {
     dat$data <- get_data(100)
   })
 
-  observeEvent(input$default_bw, {
-      updateSliderInput(session, "bandwidth", value=density(dat$data)$bw)
-  })
-
   output$plot <- renderPlot({
 
-    x <- dat$data
-    d <- density(x, bw=input$bandwidth)
-
-    par(mar=c(4,4,0,0.5))
-    plot(NULL, xlim=range(x), ylim=c(-0.02,0.5), xlab="Data", main="", ylab="Frequency")
-    points(x, rep(-0.02,length(x)), pch=4)
-    lines(d$x, d$y, col="black", lwd="3")
+    ggplot(dat$data) +
+      geom_density(aes(x=r), adjust=10^input$adjust) +
+      geom_rug(aes(x=r)) +
+      scale_x_continuous(expand=c(0,0)) +
+      scale_y_continuous(expand=c(0,0,0.05,0)) +
+      theme_bw(base_size = 15) +
+      labs(x="Data")
   })
 
 })
--- kde/ui.R
+++ kde/ui.R
@@ -1,11 +1,43 @@
 library(shiny)
 
+
+# logifySlider javascript function
+JS.logify <-
+  "
+// function to logify a sliderInput
+function logifySlider (sliderId, sci = false) {
+  if (sci) {
+    // scientific style
+    $('#'+sliderId).data('ionRangeSlider').update({
+      'prettify': function (num) { return ('10<sup>'+num+'</sup>'); }
+    })
+  } else {
+    // regular number style
+    $('#'+sliderId).data('ionRangeSlider').update({
+      'prettify': function (num) { return (Math.round((Math.pow(10, num) + Number.EPSILON) * 100) / 100); }
+    })
+  }
+}"
+
+# call logifySlider for each relevant sliderInput
+JS.onload <-
+  "
+// execute upon document loading
+$(document).ready(function() {
+  // wait a few ms to allow other scripts to execute
+  setTimeout(function() {
+    // include call for each slider
+    logifySlider('adjust', sci = false)
+  }, 5)})
+"
+
 shinyUI(fluidPage(
+  tags$head(tags$script(HTML(JS.logify))),
+  tags$head(tags$script(HTML(JS.onload))),
   fluidRow(
     column(width=8, plotOutput("plot")),
     column(width=4, wellPanel(
-      sliderInput("bandwidth", label="Bandwidth", min=0.05, max=2, value=0.4, step=0.01),
-      actionButton("default_bw", "'Optimal' Bandwidth"),
+      sliderInput("adjust", label="Bandwidth adjustment", min=-1, max=1, value=0, step=0.05),
       actionButton("new", "New Dataset")
     ))
   )
